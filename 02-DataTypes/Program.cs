9533f7936f82a9e371358f1d36b80604dec14adc
nimraabbasi037-glitch <nimraabbasi037@gmail.com>
2026-08-02 04:01:34 +0500

    Add Program.cs with basic data types example

---
+++ 02-DataTypes/Program.cs
@@ -0,0 +1,9 @@
+string name = "Nimra";
+int age = 20;
+double marks = 85.5;
+bool isStudent = true;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(marks);
+Console.WriteLine(isStudent);
