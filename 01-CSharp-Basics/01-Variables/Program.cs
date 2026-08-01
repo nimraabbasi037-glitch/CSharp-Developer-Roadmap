9be9a68f39c695a0e32bd1dde8764a44f139e599
nimraabbasi037-glitch <nimraabbasi037@gmail.com>
2026-08-02 03:33:46 +0500

    Create Program.cs for basic variable display
    Initial implementation of a C# console application that displays a name, age, and course.

---
+++ 01-CSharp-Basics/01-Variables/Program.cs
@@ -0,0 +1,18 @@
+namespace ConsoleApp_CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nimra";
+            int age = 20;
+            string course =  "C#"; 
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(course);
+
+            Console.ReadLine();
+        }
+    }
+}
