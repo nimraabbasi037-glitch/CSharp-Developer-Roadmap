d11edc0543c2be9e4ee0b77bcc7cae0e5dd33c12
nimraabbasi037-glitch <nimraabbasi037@gmail.com>
2026-08-02 16:32:23 +0500

    Add comparison operators example in ComparisonOperators.cs
    Implement comparison operators and display results.

---
+++ 03-Operators/ComparisonOperators.cs
@@ -0,0 +1,13 @@
+// =======================
+// Comparison Operators
+// =======================
+
+int num1 = 10;
+int num2 = 5;
+
+Console.WriteLine("num1 == num2 : " + (num1 == num2));
+Console.WriteLine("num1 != num2 : " + (num1 != num2));
+Console.WriteLine("num1 > num2  : " + (num1 > num2));
+Console.WriteLine("num1 < num2  : " + (num1 < num2));
+Console.WriteLine("num1 >= num2 : " + (num1 >= num2));
+Console.WriteLine("num1 <= num2 : " + (num1 <= num2));
