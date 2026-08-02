bc0af3d727b889d82610d71c3e6d22d725510a36
nimraabbasi037-glitch <nimraabbasi037@gmail.com>
2026-08-02 16:38:33 +0500

    Add Logical Operators demonstration in C#

---
+++ 03-Operators/LogicalOperators.cs
@@ -0,0 +1,16 @@
+// =======================
+// Logical Operators
+// =======================
+
+bool isStudent = true;
+bool hasFeePaid = false;
+
+Console.WriteLine("AND (&&) Operator");
+Console.WriteLine(isStudent && hasFeePaid);
+
+Console.WriteLine("OR (||) Operator");
+Console.WriteLine(isStudent || hasFeePaid);
+
+Console.WriteLine("NOT (!) Operator");
+Console.WriteLine(!isStudent);
+Console.WriteLine(!hasFeePaid);
