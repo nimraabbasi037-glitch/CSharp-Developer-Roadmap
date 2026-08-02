548578552e09a0d6f6d79f2ae536c6ee257d3b25
nimraabbasi037-glitch <nimraabbasi037@gmail.com>
2026-08-02 05:27:21 +0500

    Add basic arithmetic operations in Program.cs

---
+++ 03-Operators/Program.cs
@@ -0,0 +1,14 @@
+int num1 = 10;
+int num2 = 5;
+
+int sum = num1 + num2;
+int difference = num1 - num2;
+int multiply = num1 * num2;
+int divide = num1 / num2;
+int remainder = num1 % 3;
+
+Console.WriteLine(sum);
+Console.WriteLine(difference);
+Console.WriteLine(multiply);
+Console.WriteLine(divide);
+Console.WriteLine(remainder);
