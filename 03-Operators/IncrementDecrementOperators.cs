7391459bcd547824b540bea4d1c418354aa94abc
nimraabbasi037-glitch <nimraabbasi037@gmail.com>
2026-08-02 16:48:39 +0500

    Add Increment and Decrement Operators example

---
+++ 03-Operators/IncrementDecrementOperators.cs
@@ -0,0 +1,19 @@
+// =======================
+// Increment and Decrement Operators
+// =======================
+
+int number = 10;
+
+Console.WriteLine("Initial Value : " + number);
+
+number++;
+Console.WriteLine("After Increment : " + number);
+
+number++;
+Console.WriteLine("After Increment Again : " + number);
+
+number--;
+Console.WriteLine("After Decrement : " + number);
+
+number--;
+Console.WriteLine("After Decrement Again : " + number);
